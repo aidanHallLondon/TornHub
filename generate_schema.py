918d66cee8c59639484b32fa37bb1e76220e1928
Aidan Hall <aidan@aidanhall.com>
2025-02-17 12:09:59 +0000

    Schema images changed to svg

--- generate_schema.py
+++ generate_schema.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine,MetaData
 conn = sqlite3.connect(DB_CONNECTPATH, detect_types=sqlite3.PARSE_DECLTYPES)
 cursor = conn.cursor()
 initDB(conn,cursor)
-updateDB(conn,cursor)
+# updateDB(conn,cursor)
 cursor.execute('''SELECT schema.type, schema.name, row_count 
                FROM sqlite_master AS Schema 
                LEFT JOIN _row_counts as rc ON rc.name = Schema.name AND rc.type = Schema.type
@@ -25,11 +25,11 @@ sql_statements = [row[0] for row in cursor.fetchall() if row[0] is not None]
 # Generate the table as a string with breaks between SQL statements
 table_string_with_breaks = "\n\n".join(sql_statements)  # Join with double newlines
 
-if not os.path.exists("data/db/images"): os.makedirs("data/db/images")
+if not os.path.exists("reports/db/images"): os.makedirs("reports/db/images")
 
 
 # Write the table string to a file
-with open("data/db/schema_dump.txt", "w") as targetFile:
+with open("reports/db/schema_dump.txt", "w") as targetFile:
     targetFile.write(table_string_with_breaks)
 
 
@@ -39,7 +39,7 @@ engine = create_engine(f'sqlite:///./{DB_CONNECTPATH}')  # Note the triple slash
 metadata = MetaData()
 metadata.reflect(bind=engine)
 
-render_er(metadata, 'reports/db/images/schema_diagram') 
+render_er(metadata, 'reports/db/images/schema_diagram.svg') 
 
 # Generate the main diagram, excluding the large tables
 exclude_list = ['faction_history'] 
@@ -47,14 +47,14 @@ filtered_metadata = MetaData()
 all_tables = metadata.tables.keys()  # Get names of all tables
 included_tables = [table for table in all_tables if table not in exclude_list]
 filtered_metadata.reflect(bind=engine, only=included_tables)
-render_er(filtered_metadata, 'reports/db/images/schema_diagram_without_factionrecord')
+render_er(filtered_metadata, 'reports/db/images/schema_diagram_without_factionrecord.svg')
 
 # Generate a separate diagram for the large table
 large_table_metadata = MetaData()
 large_table_metadata.reflect(bind=engine, only=exclude_list)
-render_er(large_table_metadata, 'reports/db/images/schema_diagram_for_factionrecords')
+render_er(large_table_metadata, 'reports/db/images/schema_diagram_for_factionrecords.svg')
 
-print('')
+print('-------')
 cursor.execute('''PRAGMA integrity_check;''')
 print(tabulate(cursor.fetchall(), headers= [desc[0] for desc in cursor.description], tablefmt="simple"))
 print('\nforeign_key_check (empty is good)')
